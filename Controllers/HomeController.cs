087d7e72df06b8dfe5c77499edd2185e364acf84
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-04-06 22:58:27 -0500

    configured program.cs to run home controller/index as default, design homepaige view, with nav links and info on page, index page opens

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,12 +4,6 @@ namespace PetAdoptionWebsite.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         //display home page
         public IActionResult Index()
--- Program.cs
+++ Program.cs
@@ -1,25 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
+//MUST bet called vefore AddControllersWithViews
+builder.Services.AddMemoryCache();
+builder.Services.AddSession();
+
 // Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
+
+//get Connection string from ToDoContext
+
+
+builder.Services.AddRouting(options =>
+{
+    options.LowercaseUrls = true;
+    options.AppendTrailingSlash = true;
+});
+
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// MUST BE CALLED before UseEndPoints
+app.UseSession();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapRazorPages();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "Static",
+    pattern: "{controller=Home}/{action}/Page/{num}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
