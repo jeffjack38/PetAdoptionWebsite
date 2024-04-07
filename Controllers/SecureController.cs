471c72558ebeed4196e3cedbbf1b1171d09919a5
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-04-07 18:06:10 -0500

    SecureController

---
+++ Controllers/SecureController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetAdoptionWebsite.Controllers
+{
+    public class SecureController : Controller
+    {
+        public SecureController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Anom()
+        {
+            try
+            {
+                //action logic here
+                return View();
+            }
+            catch (Exception ex)
+            {
+                // Log the exception 
+                TempData["error"] = "An error occurred while processing the request.";
+                return RedirectToAction("Index"); // Redirect to index
+            }
+        }
+    }
+}
