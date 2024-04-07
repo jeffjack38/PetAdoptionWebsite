85a696d2b274abc3eeb72051f0fed46dbd5ff396
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-04-06 22:25:08 -0500

    added index action and logger for home controller

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,6 +4,14 @@ namespace PetAdoptionWebsite.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        //display home page
         public IActionResult Index()
         {
             return View();
