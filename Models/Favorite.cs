79488427708cb965d1eaaece4b19af26b55c4883
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-04-07 16:33:23 -0500

    add pet.cs and favorite.cs to model

---
+++ Models/Favorite.cs
@@ -0,0 +1,13 @@
+﻿namespace PetAdoptionWebsite.Models
+{
+    public class Favorite
+    {
+        public int Id { get; set; }
+
+        public string UserId { get; set; }
+        public User User { get; set; }
+
+        public int PetId { get; set; }
+        public Pet Pet { get; set; }
+    }
+}
---
+++ Models/Pet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PetAdoptionWebsite.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Species { get; set; }
+
+        [Required]
+        [Range(0, 50)]
+        public int Age { get; set; }
+
+        public string BondedBuddyStatus { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public string SpecialCareInstructions { get; set; }
+
+        // Navigation property for favorites
+        public ICollection<Favorite>? Favorites { get; set; }
+
+        public bool IsAdopted { get; set; }
+
+    }
+}
