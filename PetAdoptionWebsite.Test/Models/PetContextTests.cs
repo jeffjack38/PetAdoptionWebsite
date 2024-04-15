3dc1a9409e5ce1adb38cd84e57c10f4e77d0625e
Jeff "Jack" Aden <jeaden38@gmail.com>
2024-04-14 20:52:20 -0500

    fixed unit test in wrong directy

---
+++ PetAdoptionWebsite.Test/Models/PetContextTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Xunit;
+using PetAdoptionWebsite.Models;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+public class PetContextTests
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public PetContextTests()
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<PetContext>(options =>
+            options.UseInMemoryDatabase("TestDatabase"), ServiceLifetime.Transient);
+
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    [Fact]
+    public void ShouldSeedPetsCorrectly()
+    {
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<PetContext>();
+
+            // Trigger the seeding
+            context.Database.EnsureCreated();
+
+            // Assert
+            Assert.Equal(20, context.Pets.Count()); 
+            var firstPet = context.Pets.FirstOrDefault(p => p.Id == 1);
+            Assert.NotNull(firstPet);
+            Assert.Equal("Fluffy", firstPet.Name);
+            Assert.Equal("Cat", firstPet.Species);
+        }
+
+        // Clean up in-memory database
+        using (var scope = _serviceProvider.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<PetContext>();
+            context.Database.EnsureDeleted();
+        }
+    }
+}
+
